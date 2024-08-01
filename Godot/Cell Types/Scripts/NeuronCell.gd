60011f8ad01b6b3c7515068da054b7632ec6af19
06gusmog <135130384+06gusmog@users.noreply.github.com>
2024-08-01 15:32:55 +0200

    Updated neuron cell
    Now the neuron cell has a constant, so the output now updates as:
    output = input * weight + constant

--- Godot/Cell Types/Scripts/NeuronCell.gd
+++ Godot/Cell Types/Scripts/NeuronCell.gd
@@ -1,11 +1,14 @@
 extends Cell
-var weight: float
 var sign: bool
+var weight: float
+var constant: float
 
 func _interpret_special_sauce(special_sauce):
-	var acquired_sauce = get_from_sauce(special_sauce, ["bool", "weight"])
-	sign = acquired_sauce[0] * 2  - 1
+	var acquired_sauce = get_from_sauce(special_sauce, ["bool", "weight", "bool", "weight"])
+	sign = acquired_sauce[0] * 2 - 1
 	weight = acquired_sauce[1] * sign
+	sign = acquired_sauce[2] * 2 - 1
+	constant = acquired_sauce[3] * sign
 
 func _update_output(input):
-	output = input * weight
+	output = input * weight + constant
