8d6eda090d70e08ea8583f82a43644fe41648d2c
06gusmog <135130384+06gusmog@users.noreply.github.com>
2024-08-04 17:32:52 +0200

    Testing UI elements
    I hope I didnt break anything lol

--- Godot/Misc/UserInterface.gd
+++ Godot/Misc/UserInterface.gd
@@ -1,17 +1,21 @@
-extends Node2D
+extends Control
 var creature_selected
-
+var rendered_creature = Image
+var connections = []
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if not creature_selected: # When there is no selected creature: break
 		return 0
-	
+	for connection in connections:
+		var strength = creature_selected.get_child(connection['From'])
+		
 
 
 func creature_clicked(creature):
 	print(creature)
 	if creature_selected:
 		creature_selected.get_child(0).get_child(0).visible = false
+	visible = true
 	creature.get_child(0).get_child(0).visible = true
 	creature_selected = creature
