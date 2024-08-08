1216a647ef3238ab456f2a4c05b8456dd7f80018
06gusmog <135130384+06gusmog@users.noreply.github.com>
2024-08-08 21:30:22 +0200

    UI touch up
    I made the UI non-intrusive and easy to click away from.

--- Godot/Misc/UserInterface.gd
+++ Godot/Misc/UserInterface.gd
@@ -3,7 +3,10 @@ var creature_selected
 var rendered_creature = Image
 var connections = []
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready():
+	visible = false
+
+
 func _process(_delta):
 	if not creature_selected: # When there is no selected creature: break
 		return 0
@@ -13,9 +16,13 @@ func _process(_delta):
 
 
 func creature_clicked(creature):
-	print(creature)
 	if creature_selected:
 		creature_selected.get_child(0).get_child(0).visible = false
 	visible = true
 	creature.get_child(0).get_child(0).visible = true
 	creature_selected = creature
+
+
+func _on_exit_button_pressed():
+	visible = false
+	creature_selected.get_child(0).get_child(0).visible = false
