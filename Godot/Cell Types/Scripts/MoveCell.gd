82d20b1c63a1f8052535600c829b5a6e33f41868
Gustav Mogren <06gusmog@mk.se>
2024-08-27 09:46:16 +0200

    Movement Cell Bugfix
    The force now rotates with the creature

--- Godot/Cell Types/Scripts/MoveCell.gd
+++ Godot/Cell Types/Scripts/MoveCell.gd
@@ -9,10 +9,10 @@ func _interpret_special_sauce(special_sauce):
 	var sauce_values = get_from_sauce(special_sauce, ["angle", "weight"])
 	angle = sauce_values[0]
 	strength = sauce_values[1] * movement_tweak
-	force = Vector2(cos(angle), sin(angle)) * strength
 	sprite_2d_2.rotation = angle - PI/2
 	tags.append('Input')
 
 func _act(input, delta):
-	get_parent().apply_impulse(input * force * delta, position)
+	var direction = Vector2(cos(angle + get_parent().rotation), sin(angle + get_parent().rotation))
+	get_parent().apply_impulse(direction * strength * input * delta, position)
 	sprite_2d_2.scale.y = strength * input / (movement_tweak * 9)
