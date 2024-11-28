1520ee0a711e9192ae34916c73f6366edeca4b4b
Dunderklanten <135130791+Dunderklanten@users.noreply.github.com>
2024-11-28 21:01:25 +0100

    A very normal commit of 681 changed files
    Now has a folder with the registry for the very long simulation

--- Godot/Lineage Tracking/lineage_view.gd
+++ Godot/Lineage Tracking/lineage_view.gd
@@ -10,21 +10,21 @@ func _on_load_and_save_all_lineages_button_down() -> void:
 	if file_name.text != '':
 		DirAccess.make_dir_absolute(SAVE_PATH + file_name.text + '/')
 		var i = 0
-		for creatureID in range(LineageLogger.creature_count):
-			if LineageLogger.get_creature(str(creatureID))[0] == "-1":
-				var relative_lineage = LineageLogger.get_relative_lineage(str(creatureID))
-				var image = LineageLogger.get_image(relative_lineage.keys()[1])
-				if image.get_width() < int(image_size_minimum.text):
-					continue
+		#for creatureID in range(LineageLogger.creature_count):
+			#if LineageLogger.get_creature(str(creatureID))[0] == "-1":
+				#var relative_lineage = LineageLogger.get_relative_lineage(str(creatureID))
+				#var image = LineageLogger.get_image(relative_lineage.keys()[1])
+				#if image.get_width() < int(image_size_minimum.text):
+					#continue
+			#
+				#image.save_png(SAVE_PATH + file_name.text + '/' + str(i) + '.png')
+				#i += 1
 			
-				image.save_png(SAVE_PATH + file_name.text + '/' + str(i) + '.png')
-				i += 1
-			
-		#for creatureID in LineageLogger.get_creature('-1')[1]:
-			#var relative_lineage = LineageLogger.get_relative_lineage(str(creatureID))
-			#var image = LineageLogger.get_image(relative_lineage.keys()[1])
-			#if image.get_width() > int(image_size_minimum.text):
-				#continue
+		for creatureID in LineageLogger.get_creature('-1')[1]:
+			var relative_lineage = LineageLogger.get_relative_lineage(str(creatureID))
+			var image = LineageLogger.get_image(relative_lineage.keys()[1])
+			if image.get_width() > int(image_size_minimum.text):
+				continue
 		
-			#image.save_png(SAVE_PATH + file_name.text + '/' + str(i) + '.png')
-			#i += 1
+			image.save_png(SAVE_PATH + file_name.text + '/' + str(i) + '.png')
+			i += 1
